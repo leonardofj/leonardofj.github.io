81e4fc1b605fed2ab8ca33a2ac6fe64cdf48398d
Leonardo Jesus <leonardofj@gmail.com>
2020-11-26 00:26:33 +0100

    icon and cleanning up

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,21 +5,21 @@ export default class About extends Component {
     return (
       <div>
         <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-12">
+              <div className="col-md-12">
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
+                  <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Me</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am </p>
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>I am </p>
                     </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </section>
       </div>
     )
