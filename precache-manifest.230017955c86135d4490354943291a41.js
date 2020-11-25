859e7696dd0228a5cfccdfb331ec2d0ae05b7460
Leonardo Jesus <leonardofj@gmail.com>
2020-11-25 23:50:28 +0100

    Updates

---
+++ precache-manifest.230017955c86135d4490354943291a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265165aabc59cb17f1c5",
+    "url": "/static/css/main.93a8d8fa.chunk.css"
+  },
+  {
+    "revision": "265165aabc59cb17f1c5",
+    "url": "/static/js/main.265165aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "19d6cb89df5eaf5076f2",
+    "url": "/static/js/2.19d6cb89.chunk.js"
+  },
+  {
+    "revision": "da14995921628b071803f7854c7a8689",
+    "url": "/index.html"
+  }
+];
