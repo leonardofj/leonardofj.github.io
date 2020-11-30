8dd9aa418b583ddb6b4f2aa94ee005d9f2bb188b
Leonardo Jesus <leonardofj@gmail.com>
2020-11-30 02:11:07 +0100

    using react icons

--- src/components/projectbox.jsx
+++ src/components/projectbox.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { MdTranslate } from 'react-icons/md'
+import { GiTicTacToe } from 'react-icons/gi'
+import { FaQuestion } from 'react-icons/fa'
+import { IconContext } from "react-icons"
 
 export default class ProjectBox extends Component {
 	render() {
@@ -6,7 +10,7 @@ export default class ProjectBox extends Component {
 			<div className="col-md-4 text-center animate-box">
 				<div className={this.props.color}>
 					<span className="icon">
-						<i className={this.props.icon} />
+						{getIcon(this.props.icon)}
 					</span>
 					<div className="desc">
 						<h3><a href={this.props.link}
@@ -17,4 +21,21 @@ export default class ProjectBox extends Component {
 			</div>
 		)
 	}
+}
+
+export const getIcon = name => {
+	var icon
+	if (name === "GiTicTacToe") {
+		icon = <GiTicTacToe />
+	} else if (name === "MdTranslate") {
+		icon = <MdTranslate />
+	} else {
+		icon = <FaQuestion />
+	}
+	return (
+		<IconContext.Provider value={{ style: { fontSize: '55px', color: "white" } }}>
+			<div>
+				{icon}
+			</div>
+		</IconContext.Provider>)
 }
